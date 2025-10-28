7bd8d5f678c821822c587a677d319e41ae026b5a
pahomovandrej <out-pakhomov.a.v@sbertech.ru>
2025-10-28 12:09:46 +0300

    update project

--- src/main/java/ru/otus/waiters/BaseWaiters.java
+++ src/main/java/ru/otus/waiters/BaseWaiters.java
@@ -4,17 +4,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 
 public class BaseWaiters {
-
     private final WebDriver driver;
 
     public BaseWaiters(WebDriver driver) {
         this.driver = driver;
     }
 
-    public boolean waitForCondition(ExpectedCondition condition, int timeToWait) {
-        WebDriverWait webDriverWait = new WebDriverWait(driver, timeToWait);
+    public boolean waitForCondition(ExpectedCondition condition, long timeToWait) {
+        WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(timeToWait));
         try {
             webDriverWait.until(condition);
             return true;
